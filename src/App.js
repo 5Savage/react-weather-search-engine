e3c13336421534d97db42dc837df25fdabadbc4c
Myah Savage <minniesavage5@gmail.com>
2023-03-09 11:18:22 -0500

    added fake data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
     <div className="container">
-      <h1>Weather App</h1>
+      
+      <Weather />
+      <footer>
+        Open source code by {" "} 
+        <a href="https://github.com/5Savage/react-weather-search-engine" target="_blank"> Myah Savage</a>
+      </footer>
     </div>
     </div>
   );
